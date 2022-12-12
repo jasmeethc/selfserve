1ef9cf54a514152eefa639ecba0652f410cdf83c
SaleemHC <120365530+SaleemHC@users.noreply.github.com>
2022-12-12 13:28:19 +0530

    Add files via upload

---
+++ uploadassets.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Upload Assets</title>
+
+    <script src="https://dynamique.hockeycurve.com/js/hc.min.js"> </script>
+<script>
+    // load component into any div, here loading into body
+    let assetManager = HC.AssetManager(document.body)
+   
+    // select the folders to be shown for uploading, here we are showing test folders, key should be acquired by using the fetch keys api
+    assetManager.showFolders([{path:'test/campaign1/300x250/',key:'testkey'},{path:'test/campaign3/300x250/',key:'testkey'}])
+</script>
+</head>
+<body>
+</body>
+</html>
