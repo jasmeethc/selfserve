155a5995a4b410b086b7183ea9bb2667f43e589b
sapnahc <120365552+sapnahc@users.noreply.github.com>
2022-12-15 13:39:33 +0530

    Add files via upload

--- module1/conn.php
+++ module1/conn.php
@@ -1,11 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "self_serve"; 
+$username = "publishe_self";
+$password = "hcselfserve22";
+$dbname = "publishe_selfserve"; 
 $connectDB = mysqli_connect($servername,$username,$password,$dbname);
 if (!$connectDB){
     die("Sorry we failed to connect: ". mysqli_connect_error());
 }
-
+// else{
+//     echo "success";
+// }
 ?>
